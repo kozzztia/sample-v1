244c5c9808c99a0898d6b2dd62976024b114c09d
Kozzztia <kozzztia@ukr.net>
2022-02-07 00:42:55 +0200

    slider/v1

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,6 +1,8 @@
 const navigationList = document.querySelector('.navigation__list')
 const navigation = document.querySelector('.navigation')
-
+const portfolioCard = document.querySelectorAll('.portfolio__card')
+const nextSlider = document.querySelector('.portfolio__slider-btn--next')
+const prevSlider = document.querySelector('.portfolio__slider-btn--prev')
 
 // -----------------------------burger-menu activated
 
@@ -11,4 +13,27 @@ navigation.onmouseleave = () => {
     navigationList.classList.remove('navigation__list--mobileVersion')
 }
 // ----------------------------------
+n = 0;
+nextSlider.onclick = () => {
+    for (let i = 0; i < portfolioCard.length; i++) {
+        portfolioCard[i].classList.remove('portfolio__card--active');
+        portfolioCard[n].classList.add('portfolio__card--active');
+    }
 
+    n++;
+    if (n === portfolioCard.length) {
+        return n = 0;
+    }
+}
+prevSlider.onclick = () => {
+    for (let k = portfolioCard.length - 1; k > -1; k--) {
+        portfolioCard[k].classList.remove('portfolio__card--active');
+        portfolioCard[n].classList.add('portfolio__card--active');
+    }
+
+    n--;
+    if (n === -1) {
+        return n = portfolioCard.length - 1;
+    }
+}
+// ------------------------------------------
